087933b1128c94bebf92275a7a22030ddd72ef71
sooncheer <handy-sun@foxmail.com>
2023-10-22 18:17:07 +0800

    checkport.sh

---
+++ shell/checkport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CHK_PORT=$1
+if ! [[ "$CHK_PORT" =~ ^[1-9][0-9]{0,4}$ && "$CHK_PORT" -le 65535 ]]; then
+    echo "Error, the port input must between 1 and 65535."
+    exit 1
+fi
+
+n=1
+while true; do
+    CONTENT=`sudo netstat -antp | grep "$CHK_PORT"`
+    if [ ${#CONTENT} -gt 3 ]; then
+        echo "port is in used!"
+        break
+    else
+        echo "check port: $CHK_PORT repeat $n."
+    fi
+
+    if [ $n -gt 5 ]; then
+        echo "port is not in use"
+        exit 1
+    fi
+    n=$(($n+1))
+    sleep 2
+done
+
