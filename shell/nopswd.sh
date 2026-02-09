172514415c510a61835be982f12e73945a5af044
sooncheer <handy-sun@foxmail.com>
2026-02-09 18:58:19 +0800

    refine at MacOS

--- shell/nopswd.sh
+++ shell/nopswd.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# description: root add a user who sudo without passwd!
-if [[ "$1" = "" || "$1" = root ]]; then 
-    echo "Input a valid and real user(but not root)!"
-    exit 1
-fi
 
-is_real_user=`cat /etc/shadow | grep $1 | cut -d: -f2 | grep -v '!\*' | wc -l`
-if [ ${is_real_user} -ne 1 ]; then 
-    echo "Not a real user!"
-    exit 1
-fi
-
-uname=$1
-filepath=/etc/sudoers.d/${uname}-pass
+uname=`whoami`
+uid=`id -u`
+filepath=/etc/sudoers.d/${uid}-pass
 if [ -e ${filepath} ]; then
     echo "${filepath} exist, do nothing."
     exit 1
 fi
 
-tee ${filepath} <<< "${uname} ALL=(ALL) NOPASSWD:ALL"
-chmod 440 ${filepath}
+if [[ "$1" = "-d" ]]; then # dry-run
+    echo "tee ${filepath} <<< \"${uname} ALL=(ALL) NOPASSWD:ALL\""
+    echo chmod 440 ${filepath}
+else
+    tee ${filepath} <<< "${uname} ALL=(ALL) NOPASSWD:ALL"
+    chmod 440 ${filepath}
+fi
