91497ceabfc145b9c7955c5e0737085c64a35236
sooncheer <handy-sun@foxmail.com>
2026-02-22 17:58:46 +0800

    rm some unused files

--- gitcfg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git config --global user.name "sooncheer"
-git config --global user.email "handy-sun@foxmail.com"
-
-git config --global core.editor "vim"
-git config --global core.autocrlf "false"
-git config --global core.safecrlf "true"
-git config --global core.pager "less -FRSX"
