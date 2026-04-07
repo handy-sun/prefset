5f77ae90e2b2d6ae069abc0f18cc2868948f46aa
sooncheer <handy-sun@foxmail.com>
2026-04-07 17:01:16 +0800

    2 services about beszel; add curltest

---
+++ shell/curltest.sh
@@ -0,0 +1,27 @@
+#!env bash
+# set -x
+
+sh_c(){
+  # echo "$@"
+  sh -c '$@'
+}
+access_https(){
+  timeout 5s curl --proto '=https' --tlsv1.2 -I -sfSL https://"$1" && echo -e "\033[32mtrue\033[0m" || echo -e "\033[31mfalse\033[0m"
+  echo
+}
+
+echo ============= www.baidu.com
+$sh_c access_https www.baidu.com
+echo ============= archlinux.org
+$sh_c access_https archlinux.org
+echo ============= www.yandex.com
+$sh_c access_https www.yandex.com
+echo ============= www.youtube.com
+$sh_c access_https www.youtube.com
+echo ============= www.github.com
+$sh_c access_https www.github.com
+echo ============= www.v2ex.com
+$sh_c access_https www.v2ex.com
+echo ============= www.google.com
+$sh_c access_https www.google.com
+
